894de5e13cbeb31ae6320ed38a540b2995851306
naveenzelarsoft <naveen.b@zelarsoft.com>
2022-01-07 11:02:56 +0530

    sample

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,32 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
-resource "aws_security_group" "terraform" {
-  name = "allow_terraform"
-  description = "Allow TF inbound traffic"
-  ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = [
-      "0.0.0.0/0"]
-   }
-  tags = {
-    Name = "dev"
-  }
-}
-resource "aws_instance" "sample" {
-  ami          = "ami-0eb5f3f64b10d3e0e"
+resource "aws_instance" "app_server" {
+  ami           = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Sample"
   }
 }
