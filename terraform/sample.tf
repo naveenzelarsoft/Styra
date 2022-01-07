f1ebbde0f7afb5e0e558927c944521e3110c7f9c
naveenzelarsoft <naveen.b@zelarsoft.com>
2022-01-07 11:21:05 +0530

    sample

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "sample" {
   ami           = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
 
